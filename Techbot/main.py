ff51e49995c8d7954b12bf0b61b0c0c2d41a22f6
Mir Tasleem <106644563+Mir-Tasleem@users.noreply.github.com>
2024-05-27 09:38:49 +0530

    included langchain agents and tools in the system

--- Techbot/main.py
+++ Techbot/main.py
@@ -2,15 +2,15 @@ import bs4
 from langchain import hub
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_chroma import Chroma
-from langchain_core.output_parsers import StrOutputParser
+# from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import ChatOpenAI
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain.tools.retriever import create_retriever_tool
 import streamlit as st
-
-
-
 
 
 def qa_model(query,key):
@@ -32,27 +32,41 @@ def qa_model(query,key):
         embeddings=OpenAIEmbeddings(openai_api_key=key)
         vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
     except:
-        st.warning("Provide your api key")
+        print("api missing")
     
 
     # Retrieve and generate using the relevant snippets of the blog.
+
     llm = ChatOpenAI(model="gpt-3.5-turbo-0125",openai_api_key=key)
     retriever = vectorstore.as_retriever()
-    prompt = hub.pull("rlm/rag-prompt")
-
-    def format_docs(docs):
-        return "\n\n".join(doc.page_content for doc in docs)
-
-
-    rag_chain = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()}
-        | prompt
-        | llm
-        | StrOutputParser()
+    retriever_tool=create_retriever_tool(
+        retriever,
+        "Transformer_search",
+        "All about transformers and prompt engineering"
     )
+    prompt = hub.pull("hwchase17/react")
+    search=DuckDuckGoSearchRun()
+    
+    tools=[search,retriever_tool]
+    agent = create_react_agent(llm=llm,tools=tools,prompt=prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools,verbose=True)
 
-    answer=rag_chain.invoke(query)
-    return answer
+    # '''
+    # comented code from last submission
+    # # def format_docs(docs):
+    # #     return "\n\n".join(doc.page_content for doc in docs)
+
+
+    # # rag_chain = (
+    # #     {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    # #     | prompt
+    # #     | llm
+    # #     | StrOutputParser()
+    # # )
+    # '''
+    
+    answer=agent_executor.invoke({"input":query})
+    return answer["output"]
 
 
 def main():
